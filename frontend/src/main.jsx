68a695515775b981bd1a0a16e4afcdb703593c7d
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-31 23:25:00 +0530

    feat(frontend): add React entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource/instrument-sans/400.css";
-import "@fontsource/instrument-sans/500.css";
-import "@fontsource/instrument-sans/600.css";
-import "@fontsource/instrument-sans/700.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/700.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
